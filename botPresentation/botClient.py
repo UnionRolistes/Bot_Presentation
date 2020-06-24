4d23c4174f1dd91f28849032038d2599386eb34e
FloconDeLune <sosolebre@gmail.com>
2020-06-24 20:00:37 +0200

    test envoie de message

--- botPresentation/botClient.py
+++ botPresentation/botClient.py
@@ -1,5 +1,21 @@
 import discord
 
+botClient = discord.Client()
+
+
+@botClient.event
+async def on_message(message):
+    if message.author == botClient.user:
+        return
+
+    if message.content.startswith('$pres'):
+        await message.channel.send(
+            '{0.author.mention} regarde dans tes messages privées pour te présenter! :D'.format(message)
+        )
+
+        await message.author.send(
+            'Voici le lien de la présentation :'
+        )
 
 class botClient(discord.Client):
 
