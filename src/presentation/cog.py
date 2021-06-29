46cba01d60e067371d4f4aca56a6806c20cc682a
MoonLyss <shaitan.lyssm@gmail.com>
2021-06-29 19:07:42 +0200

    Add get_credits, get_name, get_version

--- src/presentation/cog.py
+++ src/presentation/cog.py
@@ -1,9 +1,13 @@
 import logging
 import sys
 import asyncio
+from importlib import resources
 
 import discord
 from discord.ext import commands, tasks
+
+import presentation.info
+
 
 class Presentation(commands.Cog):
     """
@@ -18,4 +22,27 @@ class Presentation(commands.Cog):
 
     @commands.command()
     async def prez(self, ctx):
+        """
+        Envoie un lien pour se présenter
+        Parameters
+        ----------
+        ctx
+
+        Returns
+        -------
+
+        """
         print("plp")
+
+    @staticmethod
+    def get_credits():
+        return resources.read_text(presentation.info, 'credits.txt')
+
+    @staticmethod
+    def get_version():
+        """ Return version.txt of bot. """
+        return resources.read_text(presentation.info, 'version.txt')
+
+    @staticmethod
+    def get_name():
+        return resources.read_text(presentation.info, 'name.txt')
