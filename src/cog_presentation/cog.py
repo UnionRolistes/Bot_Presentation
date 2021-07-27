3798ad0b2fca21d0937809fa6472a940730464d7
Lyss <shaitan.lyssm@gmail.com>
2021-07-27 05:37:19 +0200

    Add localization

--- src/cog_presentation/cog.py
+++ src/cog_presentation/cog.py
@@ -4,10 +4,13 @@ from importlib import resources
 import discord
 from discord.ext import commands
 import urpy
-from urpy import utils
+from urpy import utils, lcl, MyBot
 import cog_presentation.info
 from cog_presentation import strings
 from cog_presentation import settings
+from utils import error_log
+
+_ = lcl
 
 
 class Presentation(urpy.MyCog):
@@ -15,10 +18,17 @@ class Presentation(urpy.MyCog):
     This cog serves the purpose of automating the making of presentations on the guild.
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot: MyBot):
         super().__init__(bot, 'cog_presentation')
 
-    @commands.command()
+        global _
+        _ = bot.localization.gettext
+
+    @commands.Cog.listener()
+    async def on_ready(self):  # TODO move to MyCog
+        utils.log("\t| Presentation started.")
+
+    @commands.command(brief=strings.jdr_brief, help=strings.jdr_help)
     async def prez(self, ctx):
         """
         Envoie un lien pour se présenter
@@ -37,8 +47,8 @@ class Presentation(urpy.MyCog):
                     webhooks = await anncmnt_channel.webhooks()
                     webhook: discord.Webhook = webhooks[0]
                 except discord.errors.Forbidden:
-                    print("ERROR:BOT| Impossible d'obtenir les webhooks.",
-                          "Le bot nécessite la permission de gérer les webhooks", file=sys.stderr)
+                    error_log("Impossible d'obtenir les webhooks.",  # TODO change to english
+                              "Le bot nécessite la permission de gérer les webhooks")
                     await ctx.author.send(strings.on_permission_error)
                 except IndexError:
                     await ctx.send(strings.on_prez_webhook_not_found.format(channel=settings.announcement_channel))
--- src/cog_presentation/strings.py
+++ src/cog_presentation/strings.py
@@ -1,19 +1,24 @@
-on_join = "Célébrations ! {user} a rejoint le JDR !"
-on_leave = "Ôrage, eau des espoirs... {user} nous a quitté. Méchant {user}."
-on_edit_without_reply = "Vous devez répondre à un message pour l'éditer !"
-on_edit_wrong_channel = "Vous ne pouvez pas éditer dans ce salon !"
-on_edit_start = "C'est parti ! Rejoignez-moi dans vos dms."
-on_edit_not_editable = "Ce message n'est pas éditable."
-on_edit_not_author = "Vous n'êtes pas l'auteur de cette présentation ! Vous ne pouvez donc pas l'éditer."
-on_edit_prompt = "\|~ Quelles informations souhaitez-vous modifier ? (**$done** pour valider, **$cancel** pour annuler) ~|"
-on_edit_unrecognized = "\|~ Information non reconnue. Veuillez réessayer. (**$done** pour valider, **$cancel** pour annuler) ~|"
-on_edit_content_prompt = "\|~ Nouveau contenu pour {info} ? ~|"
-on_edit_invalid = ""
-on_edit_success = "\|~ Félicitations, le message a été édité avec brio. ~|"
-on_edit_cancel = "\|~ Annulation. ~|"
-on_prez = "Un lien a été envoyé dans vos DM pour vous présenter dignement !"
-on_prez_link = "Voici le lien de présentation :\n{link}"
-on_prez_dm_channel = "Cette commande n'est utilisable que dans un serveur !"
-on_prez_channel_not_found = "*Impossible*. Le salon ** {channel} ** n'existe pas. "
-on_prez_webhook_not_found = "*Impossible. Le salon ** {channel} ** ne dispose pas d'un webhook.*"
-on_permission_error = "Erreur, le bot n'a pas les permissions requises. Contactez l'administrateur."
+from urpy import lcl
+
+jdr_brief = lcl("Sends a link to create your presentation")
+jdr_help = jdr_brief
+# TODO edit presentation
+# on_edit_without_reply = "Vous devez répondre à un message pour l'éditer !"
+# on_edit_wrong_channel = "Vous ne pouvez pas éditer dans ce salon !"
+# on_edit_start = "C'est parti ! Rejoignez-moi dans vos dms."
+# on_edit_not_editable = "Ce message n'est pas éditable."
+# on_edit_not_author = "Vous n'êtes pas l'auteur de cette présentation ! Vous ne pouvez donc pas l'éditer."
+# on_edit_prompt = "\|~ Quelles informations souhaitez-vous modifier ? (**$done** pour valider, **$cancel** pour annuler) ~|"
+# on_edit_unrecognized = "\|~ Information non reconnue. Veuillez réessayer. (**$done** pour valider, **$cancel** pour annuler) ~|"
+# on_edit_content_prompt = "\|~ Nouveau contenu pour {info} ? ~|"
+# on_edit_invalid = ""
+# on_edit_success = "\|~ Félicitations, le message a été édité avec brio. ~|"
+# on_edit_cancel = "\|~ Annulation. ~|"
+
+on_prez = lcl("A link has been sent into your DMs to introduce yourself !")
+on_prez_link = lcl("Here is the link to create your presentation :\n{link}")
+on_prez_channel_not_found = lcl("*Impossible.*. The following channel does not exist : {channel}.")
+on_prez_dm_channel = lcl("This command is only usable in a server !")
+on_prez_webhook_not_found = lcl("*Impossible. The following channel does not own a webhook : **{channel}**.*")
+on_permission_error = lcl("Error, the bot requires more permission. Please contact the admin.")
+
