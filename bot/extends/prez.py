98dee83858f285427e7aa9258340dddc6e619d7a
Mortifia <amela57290@gmail.com>
2022-12-23 17:22:58 +0100

    add cred, add version

--- bot/extends/prez.py
+++ bot/extends/prez.py
@@ -1,10 +1,18 @@
 import asyncio
+import os
 import discord
 from discord.ext import commands
 
+from dotenv import load_dotenv
+
+load_dotenv()
+VERSION = os.getenv('BOT_PREZ_VERSION')
+PREZ_URL = os.getenv('URL_SITE_PRESENTATION')
+print(f'VERSION_PREZ : {VERSION}')
+
 
 class Prez(commands.Cog, name='prez'):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self._last_member = None
 
@@ -13,21 +21,23 @@ class Prez(commands.Cog, name='prez'):
 
     @commands.command(name="prez")
     async def prez(Self, event):
-        embed = discord.Embed(url="http://presentation.unionrolistes.fr/?webhook=https://discord.com/api/webhooks/875068900612665396/DJusy0eGs9Xyx2os-dodBVfWia2fbhfBzfmnDM9g-30ozoFYAuZBHVXaD9TKaC1wwBwg",
-                              description="⬆️ Here is the link to create your presentation.", title="Union Roliste - Presentation", color=0x0CC1EE)
-        embed.set_author(name=event.author.display_name,
-                         icon_url=event.author.avatar)
-        DATA = ["**:pen_ballpoint:  Nom\n**", "**:pen_ballpoint:  Prenom\n**", ":round_pushpin:  **Address\n**", ":telephone:   **N° Telephone\n**",
-                ":postbox: **Code postal\n**", "**:computer:  Support (Windows / Linux / Mac)\n**", "**Expérience en programmation\n**"]
-        embed.add_field(
-            name="**\n**", value="**───────────────────────────────**", inline=False)
-        embed.set_footer(text="Union Roliste dev presentation.",
-                         icon_url="https://avatars.githubusercontent.com/u/62179928?s=200&v=4")
-        embed.set_thumbnail(
-            url="https://avatars.githubusercontent.com/u/62179928?s=200&v=4")
-        # envoie un message de presentation privée à l'auteur qui a fait a commandes
-        await event.author.send(embed=embed)
+        await event.author.send(f'veuiller suivre le lient suivant : {PREZ_URL}')
 
 
 async def setup(bot):
     await bot.add_cog(Prez(bot))
+
+
+# loaded in bot\extends\_base\base.py
+def version():
+    return f'URBot_ version : {VERSION}'
+
+
+# loaded in bot\extends\_base\base.py
+def _credits():
+    # get the path of the file
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    # get the credits file
+    with open(f'{pwd}/credits.txt', 'r') as f:
+        credits = f.read()
+        return credits
