30a200c6c5fb0fffc0076a874084869619271e15
Younes Bourakadi <younes.bourakadi@gmail.com>
2023-09-04 21:21:47 +0200

    Add  command draft

--- bot/extends/prez.py
+++ bot/extends/prez.py
@@ -6,12 +6,13 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-VERSION = os.getenv('BOT_PREZ_VERSION')
-PREZ_URL = os.getenv('URL_SITE_PRESENTATION')
-print(f'VERSION_PREZ : {VERSION}')
+VERSION = os.getenv("BOT_PREZ_VERSION")
+PREZ_URL = os.getenv("URL_SITE_PRESENTATION")
+presentation_channels = {}
+print(f"VERSION_PREZ : {VERSION}")
 
 
-class Prez(commands.Cog, name='Prez'):
+class Prez(commands.Cog, name="Prez"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self._last_member = None
@@ -19,37 +20,34 @@ class Prez(commands.Cog, name='Prez'):
     async def cog_load(self):  # called when the cog is loaded
         print(self.__class__.__name__ + " is loaded")
 
-    @commands.command(name="prez", help='envoie un lien pour se présenter', aliases=['presentation'])
+    @commands.command(
+        name="prez", help="envoie un lien pour se présenter", aliases=["presentation"]
+    )
     async def prez(self, event):
-        await event.author.send(f'Veuillez suivre le lien suivant : {PREZ_URL}')
+        await event.author.send(f"Veuillez suivre le lien suivant : {PREZ_URL}")
         channel = event.channel
-        await channel.send("Un lien a été envoyé dans vos messages privés pour vous présenter dignement !")
+        await channel.send(
+            "Un lien a été envoyé dans vos messages privés pour vous présenter dignement !"
+        )
 
-
+    @commands.command(
+        name="set",
+        help="définir un canal pour les présentations avec la commande `$set prez #nomduchannel`",
+    )
+    async def set_presentation_channel(self, ctx, channel_name: str):
+        if channel_name.startswith("#"):
+            channel_name = channel_name[1:]
+        
+        channel = discord.utils.get(ctx.guild.text_channels, name=channel_name)
+        
+        if channel:
+            presentation_channels[ctx.guild.id] = channel.id
+            await ctx.send("Canal de présentation défini sur #{channel_name}.")
+        else:
+            await ctx.send(
+                "Le canal spécifié n'existe pas ou n\'est pas un salon textuel valide."
+            )
 
 
 async def setup(bot):
     await bot.add_cog(Prez(bot))
-
-
-# loaded in bot\extends\_base\base.py
-def version():
-    try:
-        # Lecture du fichier
-        with open('version.txt', 'r') as f:
-            VERSION = f.read()
-        return f'URBot_base version : {VERSION}'
-    except FileNotFoundError:
-        return 'Erreur : le fichier version.txt est introuvable.'
-    except Exception as e:
-        return f'Erreur lors de la lecture du fichier : {str(e)}'
-
-
-# loaded in bot\extends\_base\base.py
-def _credits():
-    # get the path of the file
-    pwd = os.path.dirname(os.path.abspath(__file__))
-    # get the credits file
-    with open(f'{pwd}/credits.txt', 'r') as f:
-        credits = f.read()
-        return credits
